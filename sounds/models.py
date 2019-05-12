49e3341cab6a04f84b613e18a78d6eddc7af8abd
JA <joshappeldev@gmail.com>
2019-05-12 12:29:20 -0400

    ran migrations

--- sounds/models.py
+++ sounds/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Sound(models.Model):
+    displayName = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
+    character = models.CharField(max_length=50)
+    charId = models.IntegerField()
+    audio = models.CharField(max_length=500)
+    date_created = models.DateTimeField(default=timezone.now)
