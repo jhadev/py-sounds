c2f242e07a53101f942a31343e9973866d9c22c6
JA <joshappeldev@gmail.com>
2019-05-14 23:36:28 -0400

    try again

--- pysounds/settings.py
+++ pysounds/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 import dj_database_url
 import dotenv
 
@@ -119,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
